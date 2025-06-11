319456cc4694246fddef39e26f478d591dab7e30
darkness <sixket4765@gmail.com>
2025-06-12 01:20:06 +0900

    init

---
+++ src/main/kotlin/com/example/feedev/FeeDevApplication.kt
@@ -0,0 +1,11 @@
+package com.example.feedev
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FeeDevApplication
+
+fun main(args: Array<String>) {
+    runApplication<FeeDevApplication>(*args)
+}
